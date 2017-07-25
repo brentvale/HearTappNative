ec1c429ba2235352e3b54154aa4af1bf2ab7a693
Brent Vale <brentvale@gmail.com>
2017-07-25 15:38:05 -0700

    adds beating heart icon to home page

--- components/home_screen.js
+++ components/home_screen.js
@@ -2,10 +2,27 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from 'react-native-button';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const LARGE_HEART_SIZE = 50;
+const SMALL_HEART_SIZE = 40;
+
 export default class HomeScreen extends Component{
 	constructor(){
 		super();
+		this.state = {
+			heartSize: SMALL_HEART_SIZE
+		}
 		this._handlePress = this._handlePress.bind(this);
+	}
+	
+	componentDidMount(){
+		this.heartSizeInterval = setInterval(() => {
+			this.setState({heartSize: LARGE_HEART_SIZE});
+			setTimeout(() => {
+				this.setState({heartSize: SMALL_HEART_SIZE});
+			}, 100);
+		}, 1000);
 	}
 	
 	_handlePress(){
@@ -13,12 +30,20 @@ export default class HomeScreen extends Component{
 	}
 	
 	render(){
+		//make heart beat extend evenly from a central point...
+		//larger heart beat should have padding that's half the size delta between large and small 
+		const paddingDelta = (LARGE_HEART_SIZE - SMALL_HEART_SIZE) / 2
+		const calculatedPadding = (this.state.heartSize === SMALL_HEART_SIZE) ? paddingDelta : 0;
 		return(
 			<View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
-				<View style={{flex: 2, flexDirection: 'row', alignItems: 'flex-end'}}>
+				<View style={{flex: 3, flexDirection: 'row', alignItems: 'flex-end'}}>
 					<Text style={styles.heading}>Hear<Text style={styles.span}>T</Text>app</Text>
 				</View>
-        
+			
+				<View style={{flex: 1, alignItems: "stretch"}}>
+			<Ionicons name="ios-heart" style={{color:'red', fontSize: this.state.heartSize, padding: calculatedPadding}}/>
+				</View>
+				
 				<View style={{flex: 1, flexDirection: 'row', alignItems: 'flex-end', margin: 20}}>
 					<Button	containerStyle={{padding:10, height:50, overflow:'hidden', borderRadius:4, backgroundColor: 'red'}}
 									style={{fontSize: 20, color: 'white', width: 200}}
